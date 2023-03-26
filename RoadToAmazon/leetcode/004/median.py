93a73dcf9f09ba5cc4ab886f36b27b3f9d487432
btran <nguyenbao1003@outlook.com>
2023-03-25 20:53:49 -0400

    WIP

---
+++ RoadToAmazon/leetcode/004/median.py
@@ -0,0 +1,11 @@
+class Soluion:
+  def getMedianSameSize(self, num1, num2):
+    
+    return 0
+
+if __main__ == '__main__':
+  sol = Solution()
+  num1 = [1, 12, 15, 26, 38]
+  num2 = [2, 13, 17, 30, 45]
+  median = sol.getMedianSameSize(num1, num2, len(num1))
+  print('median', median) 
