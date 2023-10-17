e23e362ed73a3c2f7bfb2fef3937ad8ce59c4357
Bao Tran <nguyenbao1003@hotmail.com>
2023-10-16 21:08:00 -0400

    Done two sum homework

--- RoadToAmazon/homework/homework_2023Oct16.py
+++ RoadToAmazon/homework/homework_2023Oct16.py
@@ -5,16 +5,44 @@ import util as Util
 util = Util.Utility()
 
 def twoSum1(arr: list[int], k: int) -> bool:
-  pass
+  N = len(arr)
+  for i, val1 in enumerate(arr):
+    for j, val2 in enumerate(arr[i+1:]):
+      if val1 + val2 == k:
+        return True
+  return False
 
 def twoSum2(arr: list[int], k: int) -> bool:
-  pass
+  N = len(arr)
+  arr.sort()
+  for i, val in enumerate(arr):
+    x = bisect.bisect_left(arr, k - val, i + 1, N)
+    if x != N and arr[x] == k - val:
+      return True
+  return False
 
 def twoSum3(arr: list[int], k: int) -> bool:
-  pass
+  N = len(arr)
+  arr.sort()
+  i, j = 0 , N - 1
+  while i < j:
+    target = arr[i] + arr[j]
+    if target > k:
+      j -= 1
+    elif target == k:
+      return True
+    else:
+      i += 1
+  return False
 
 def twoSum4(arr: list[int], k: int) -> bool:
-  pass
+  N = len(arr)
+  hashMap = {}
+  for i, val in enumerate(arr):
+    if hashMap.get(k - val, 0):
+      return True
+    hashMap[val] = 1
+  return False
 
 def binarySearch(arr: list[int], k: int):
   pass
